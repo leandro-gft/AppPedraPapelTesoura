aa6b6e9c6ce8f984e81ab719cd8f670967bad550
leandro-gft <lpsacchi@gmail.com>
2020-05-08 22:18:20 -0300

    Adaptações para Jokepô Spock. Algumas melhorias de interface.

--- app/src/main/java/br/com/gft/apppedrapapeltesoura/MainActivity.java
+++ app/src/main/java/br/com/gft/apppedrapapeltesoura/MainActivity.java
@@ -15,8 +15,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    // private ViewHolder mViewHolder = new ViewHolder();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,24 +34,42 @@ public class MainActivity extends AppCompatActivity {
         opcaoSelecionada("tesoura");
     }
 
-    public void opcaoSelecionada(String opcaoEscolhida) {
+    public void selecionarLagarto(View view) {
+        opcaoSelecionada("lagarto");
+    }
 
+    public void selecionarSpock(View view) {
+        opcaoSelecionada("spock");
+    }
+
+    public void opcaoSelecionada(String opcaoEscolhida) {
         ImageView imagem = findViewById(R.id.img_escolha_app);
+        ImageView imagem2 = findViewById(R.id.img_escolha_app2);
         TextView text = findViewById(R.id.text_sua_escolha);
 
-        String[] opcoes = {"pedra", "papel", "tesoura"};
-        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura", "lagarto", "spock"};
+        int numero = new Random().nextInt(5);
         String escolhaApp = opcoes[numero];
+
+        switch (opcaoEscolhida){
+            case "pedra": imagem2.setImageResource(R.drawable.pedra); break;
+            case "papel": imagem2.setImageResource(R.drawable.papel); break;
+            case "tesoura": imagem2.setImageResource(R.drawable.tesoura); break;
+            case "lagarto": imagem2.setImageResource(R.drawable.lagarto); break;
+            case "spock": imagem2.setImageResource(R.drawable.spock); break;
+        }
 
         switch (escolhaApp) {
             case "pedra": imagem.setImageResource(R.drawable.pedra); break;
             case "papel": imagem.setImageResource(R.drawable.papel); break;
             case "tesoura": imagem.setImageResource(R.drawable.tesoura); break;
+            case "lagarto": imagem.setImageResource(R.drawable.lagarto); break;
+            case "spock": imagem.setImageResource(R.drawable.spock); break;
         }
 
-        if (opcaoEscolhida=="papel" && escolhaApp == "pedra" || opcaoEscolhida=="tesoura" && escolhaApp == "papel" || opcaoEscolhida=="pedra" && escolhaApp == "tesoura" ){
+        if (opcaoEscolhida=="papel" && escolhaApp == "pedra" || opcaoEscolhida=="pedra" && escolhaApp == "lagarto" || opcaoEscolhida=="lagarto" && escolhaApp == "spock" || opcaoEscolhida=="tesoura" && escolhaApp == "lagarto" || opcaoEscolhida=="lagarto" && escolhaApp == "papel" || opcaoEscolhida=="papel" && escolhaApp == "spock" || opcaoEscolhida=="spock" && escolhaApp == "pedra" || opcaoEscolhida=="spock" && escolhaApp == "tesoura" || opcaoEscolhida=="tesoura" && escolhaApp == "papel" || opcaoEscolhida=="pedra" && escolhaApp == "tesoura" ){
             text.setText("Você ganhou!");
-        } else if (opcaoEscolhida=="pedra" && escolhaApp == "pedra" || opcaoEscolhida=="papel" && escolhaApp == "papel" || opcaoEscolhida=="tesoura" && escolhaApp == "tesoura" ){
+        } else if (opcaoEscolhida=="pedra" && escolhaApp == "pedra" || opcaoEscolhida=="lagarto" && escolhaApp == "lagarto" || opcaoEscolhida=="spock" && escolhaApp == "spock" || opcaoEscolhida=="papel" && escolhaApp == "papel" || opcaoEscolhida=="tesoura" && escolhaApp == "tesoura" ){
             text.setText("Empatou!");
         } else {
             text.setText("Você perdeu!");
